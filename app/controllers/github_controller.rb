f20cbc781237cdc84024484ac16553683f14dec2
Graham Thompson <grwthomps@gmail.com>
2019-12-08 14:26:36 -0700

    Refactor GithubController create action to save github_username to User
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,7 +1,8 @@
 class GithubController < ApplicationController
   def create
     user_info = request.env['omniauth.auth']
-    current_user.update!(:github_token => user_info[:credentials][:token])
+    current_user.update!(:github_token => user_info[:credentials][:token],
+                         :github_username => user_info[:extra][:raw_info][:login])
     redirect_to dashboard_path
   end
 end
