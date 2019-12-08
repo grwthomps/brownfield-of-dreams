80abfcbde8f68191a66960721990e3a8def4cd6d
Tyla Devon <tyladevon@gmail.com>
2019-12-07 17:04:11 -0700

    Refactor Welcome and Tutorial Controllers to check if User is logged in
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,6 +1,19 @@
 class TutorialsController < ApplicationController
   def show
     tutorial = Tutorial.find(params[:id])
-    @facade = TutorialFacade.new(tutorial)
+    if tutorial.classroom == true && !current_user
+      flash[:error] = "Please log in to see this content"
+      redirect_to '/'
+    else
+      @facade = TutorialFacade.new(tutorial)
+    end
+  end
+
+  def index
+    if current_user
+      @tutorials = Tutorial.all
+    else
+      @tutorials = Tutorial.where("classroom = false")
+    end
   end
 end
