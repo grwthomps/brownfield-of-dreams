a9ab27f9d16c8a8d2a7cc8d2190c884aceb63f4e
Graham Thompson <grwthomps@gmail.com>
2019-12-09 07:59:15 -0700

    Add assign_id method to GithubUser poro

--- app/poros/github_user.rb
+++ app/poros/github_user.rb
@@ -1,9 +1,17 @@
 class GithubUser
-  attr_reader :login, :html_url
+  attr_reader :login, :html_url, :id
 
   def initialize(login, html_url)
     @login = login
     @html_url = html_url
-    @id = User.find_by(github_username: login).id
+    @id = assign_id
+  end
+
+  def assign_id
+    if User.find_by(github_username: @login) != nil
+      @id = User.find_by(github_username: @login).id
+    else
+      @id = nil
+    end
   end
 end
