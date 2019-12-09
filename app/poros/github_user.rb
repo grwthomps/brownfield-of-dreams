3129af30d1068b81054f2db7747c3ea38fbbd829
Graham Thompson <grwthomps@gmail.com>
2019-12-09 15:12:42 -0700

    Refactor Github User poro to remove assign_id method
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>

--- app/poros/github_user.rb
+++ app/poros/github_user.rb
@@ -1,17 +1,9 @@
 class GithubUser
   attr_reader :login, :html_url, :id
 
-  def initialize(login, html_url)
+  def initialize(login, html_url, user = nil)
     @login = login
     @html_url = html_url
-    @id = assign_id
-  end
-
-  def assign_id
-    if User.find_by(github_username: @login) != nil
-      @id = User.find_by(github_username: @login).id
-    else
-      @id = nil
-    end
+    @id = user ? user.id : nil
   end
 end
