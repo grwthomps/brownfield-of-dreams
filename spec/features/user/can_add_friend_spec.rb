1fd27a7a25abd6b16f387b45de6673ddb0a74390
Tyla Devon <tyladevon@gmail.com>
2019-12-09 16:23:57 -0700

    Refactor spec to test that user cannot add friend if not brownfield user.
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>

--- spec/features/user/can_add_friend_spec.rb
+++ spec/features/user/can_add_friend_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 describe "As a logged in User" do
   before(:each) do
     VCR.use_cassette('adding_friends') do
-      user = create(:user, github_token: ENV["Github_access_token"])
+      user = create(:user, github_username: "grwthomps", github_token: ENV["Github_access_token"])
+      friend = create(:user, github_token: ENV["friend_access_token"], github_username: "tyladevon")
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       visit '/dashboard'
     end
+  end
 
   context "I see a button only next to Gituhub followers who are also Brownsfield users" do
     it "I can add friend" do
@@ -16,29 +18,19 @@ describe "As a logged in User" do
         expect(page).to have_button "Add Friend"
       end
       within "#follower-1" do
-        expect(page).to have_link
+        expect(page).to_not have_button "Add Friend"
       end
-      within "#follower-2" do
-        expect(page).to have_link
-      end
-
-    end
-
-    it "cannot add friend of follower who is not a Brownsfield user" do
-
     end
   end
-end
 
-describe "As a logged in User" do
   context "I see a button only next to a Gituhub user I am following who is also a Brownsfield users" do
     it "I can add friend" do
-
+      within "#follow-1" do
+        expect(page).to have_button "Add Friend"
+      end
+      within "#follow-0" do
+        expect(page).to_not have_button "Add Friend"
+      end
     end
-
-    it "cannot add friend of someone I am following who is not a Brownsfield user" do
-
-    end
-    end 
   end
 end
