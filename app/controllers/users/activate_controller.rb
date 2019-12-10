7e606223d4d41df8040859430f242b4061ca485e
Graham Thompson <grwthomps@gmail.com>
2019-12-10 15:40:54 -0700

    Create user namespaced activate controller with show action
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>

---
+++ app/controllers/users/activate_controller.rb
@@ -0,0 +1,12 @@
+class Users::ActivateController < ApplicationController
+  def show
+    user = User.find(params[:id])
+    user.update!(status: 'active')
+    if user.save
+      flash[:success] = "Thank you, your account is now activated."
+    else
+      flash[:error] = "Whoops, something went wrong. Please try to activate again."
+    end
+    redirect_to dashboard_path
+  end
+end
