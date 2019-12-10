905a10a679f8ca479be50e0f41dff64105e328c1
Tyla Devon <tyladevon@gmail.com>
2019-12-09 18:54:19 -0700

    (Incomplete) working on bookmarks order by

--- spec/factories/videos.rb
+++ spec/factories/videos.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     title { Faker::Pokemon.name }
     description { Faker::SiliconValley.motto }
     video_id { Faker::Crypto.md5 }
+    position { Faker::Random }
     tutorial
   end
 end
