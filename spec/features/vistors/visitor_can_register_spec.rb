707bb77fa596c982cc4c4aa895158f3ef16ef02f
Graham Thompson <grwthomps@gmail.com>
2019-12-10 11:55:58 -0700

    Create tests for attempting to register with blank input fields
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>

--- spec/features/vistors/visitor_can_register_spec.rb
+++ spec/features/vistors/visitor_can_register_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe 'vister can create an account', :js do
-  it ' visits the home page' do
+describe 'As a visitor', :js do
+  it 'I can register to create an account' do
     email = 'jimbob@aol.com'
     first_name = 'Jim'
     last_name = 'Bob'
@@ -32,5 +32,49 @@ describe 'vister can create an account', :js do
     expect(page).to have_content(first_name)
     expect(page).to have_content(last_name)
     expect(page).to_not have_content('Sign In')
+  end
+
+  it 'I cannot register if fields are blank' do
+    email = 'jimbob@aol.com'
+    first_name = 'Jim'
+
+    visit '/register'
+
+    fill_in 'user[email]', with: email
+    fill_in 'user[first_name]', with: first_name
+
+    click_on 'Create Account'
+
+    expect(current_path).to eq('/register')
+
+    expect(page).to have_content("Last name can't be blank and Password can't be blank")
+  end
+
+  it "I see a flash message indicating I haven't activated account after registering" do
+    email = 'jimbob@aol.com'
+    first_name = 'Jim'
+    last_name = 'Bob'
+    password = 'password'
+    password_confirmation = 'password'
+
+    visit '/'
+
+    click_on 'Sign In'
+
+    expect(current_path).to eq(login_path)
+
+    click_on 'Sign up now.'
+
+    expect(current_path).to eq(new_user_path)
+
+    fill_in 'user[email]', with: email
+    fill_in 'user[first_name]', with: first_name
+    fill_in 'user[last_name]', with: last_name
+    fill_in 'user[password]', with: password
+    fill_in 'user[password_confirmation]', with: password
+
+    click_on'Create Account'
+
+    expect(page).to have_content("This account has not yet been activated. Please check your email.")
   end
 end
