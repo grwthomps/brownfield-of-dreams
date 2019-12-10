a38904d22d0d2b1398288ed582f9625ab9c85e38
Graham Thompson <grwthomps@gmail.com>
2019-12-10 11:56:49 -0700

    Refactor create action in user controller to show flash messages upon error
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,17 +17,20 @@ class UsersController < ApplicationController
     user = User.create(user_params)
     if user.save
       session[:user_id] = user.id
+      flash[:success] = "Logged in as #{user.first_name}."
+      flash[:success] = "This account has not yet been activated. Please check your email."
       redirect_to dashboard_path
     else
-      flash[:error] = 'Username already exists'
-      render :new
+      flash[:error] = user.errors.full_messages.to_sentence
+      # render :new
+      redirect_to '/register'
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:email, :first_name, :last_name, :password)
+    params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation)
   end
 
 end
