f36a84d134da02021a980a532a96d7d52f1b1288
Graham Thompson <grwthomps@gmail.com>
2019-12-10 15:37:36 -0700

    Generate migration files for adding status column to user table
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_08_213125) do
+ActiveRecord::Schema.define(version: 2019_12_10_222322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2019_12_08_213125) do
     t.datetime "updated_at", null: false
     t.string "github_token"
     t.string "github_username"
+    t.string "status", default: "inactive"
     t.index ["email"], name: "index_users_on_email"
   end
 
