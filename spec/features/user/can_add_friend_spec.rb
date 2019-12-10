e943431931067ee7aa9a729e555323a48e0a85ca
Tyla Devon <tyladevon@gmail.com>
2019-12-09 17:26:30 -0700

    Refactor test for sad path testing for Add Friend button. Remove VCR

--- spec/features/user/can_add_friend_spec.rb
+++ spec/features/user/can_add_friend_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
-describe "As a logged in User" do
+describe "As a logged in User", :vcr do
   before(:each) do
-    VCR.use_cassette('adding_friends') do
-      user = create(:user, github_username: "grwthomps", github_token: ENV["Github_access_token"])
-      friend = create(:user, github_token: ENV["friend_access_token"], github_username: "tyladevon")
+    user = create(:user, github_username: "grwthomps", github_token: ENV["Github_access_token"])
+    friend = create(:user, github_token: ENV["friend_access_token"], github_username: "tyladevon")
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
-      visit '/dashboard'
-    end
+    visit '/dashboard'
   end
+
 
   context "I see a button only next to Gituhub followers who are also Brownsfield users" do
     it "I can add friend" do
@@ -18,6 +17,15 @@ describe "As a logged in User" do
         expect(page).to have_button "Add Friend"
       end
       within "#follower-1" do
+        expect(page).to_not have_button "Add Friend"
+      end
+      within "#follower-0" do
+        click_button 'Add Friend'
+      end
+
+      expect(current_path).to eq(dashboard_path)
+
+      within "#follower-0" do
         expect(page).to_not have_button "Add Friend"
       end
     end
