b8350b21ce35f773da139b231f6aed52c119e0cd
Tyla Devon <tyladevon@gmail.com>
2019-12-10 20:39:23 -0700

    Refactor url for production in Heroku.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
-    @url  = "localhost:3000/users/#{@user.id}/activate"
+    @url  = "https://shrouded-island-82497.herokuapp.com/users/#{@user.id}/activate"
     mail(to: @user.email, subject: 'Welcome to Turing Tutorials!')
   end
 end
