4d7abf7b358da300b54ea7df8311b67336950c22
Graham Thompson <grwthomps@gmail.com>
2019-12-11 11:20:35 -0700

    Remove unnecessary url instance variable in user mailer
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,6 @@
 class UserMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
-    @url  = "localhost:3000/users/#{@user.id}/activate"
     mail(to: @user.email, subject: 'Welcome to Turing Tutorials!')
   end
 end
