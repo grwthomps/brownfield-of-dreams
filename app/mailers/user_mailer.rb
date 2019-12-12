ba94ddd920c348f8c4b2e8566810db5014956589
Graham Thompson <grwthomps@gmail.com>
2019-12-11 18:17:40 -0700

    Create invite_email method in user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,4 +3,10 @@ class UserMailer < ApplicationMailer
     @user = params[:user]
     mail(to: @user.email, subject: 'Welcome to Turing Tutorials!')
   end
+
+  def invite_email(user, current_user)
+    @user = user
+    @current_user_name = "#{current_user.first_name} #{current_user.last_name}"
+    mail(to: user[:email], subject: "You've been invited to Turing Tutorials!")
+  end
 end
